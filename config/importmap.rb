dcce6ea8c9dba82c6bc0f0388156d9d6d5603ec8
franka-c <franka.cvetko@decode.agency>
2025-03-25 21:47:18 +0100

    hopefully final commit for logout

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
 pin "@hotwired/turbo-rails", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo-rails@7.3.0/+esm"
-pin "@rails/ujs", to: "rails-ujs.js"
+pin "@rails/ujs", to: "https://cdn.jsdelivr.net/npm/@rails/ujs@7.0.4/+esm"
 pin "application"
 pin_all_from "app/javascript", under: "application"
